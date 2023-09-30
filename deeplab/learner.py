f163d5b35c9ad57889a9976e932e946d9503a067
이상윤 <speedp001@naver.com>
2023-10-01 02:40:41 +0900

    <deeplab_project - 학습 실행 코드>
    주석 추가

--- deeplab/learner.py
+++ deeplab/learner.py
@@ -46,19 +46,43 @@ class SegLearner:
             # mIoU 계산을 위한 IoU 총합치 저장 변수
             
             for i, (inputs, labels) in enumerate(tqdm(self.train_dataloader)):
+                # 이미지
                 inputs = inputs.float().to(self.device)
+                # print("input", inputs)
+                # 정답지 (segementation image)
                 labels = labels.long().to(self.device)
+                # print("label", labels)
+                """
+                여기서 inputs는 4차원 labels는 3차원이다. 이유는 labels에서는 배경과 객체 이 두개의 클래스만 구분한다면 클래스 값이 0또는 1로 구분될 수 있기 때문에 차원을 늘릴 필요가 없다.
+                0이외의 숫자에서는 숫자를 부여하여 각 클래스의 번호를 새긴다.
+                
+                deeplab 모델에서는 0~20번호의 숫자가 클래스 개수이고 255는 배경을 의미한다.
+                """
                 
                 self.optimizer.zero_grad()  # 가중치 업데이트를 위한 optimizer 초기화
                 
                 outputs = self.model(inputs)  # 순전파
+
                 outputs = outputs["out"]  # deeplab은 output이 dict 형태로 쓸 수 있도록 나오므로, 출력치 key로 받아옴
+                # 입력 이미지가 520x520 픽셀이면 "out"의 형태는 (배치 크기, 클래스 수, 520, 520)
+                
+                # # outputs 형태 확인
+                # print(outputs.shape)
+                
+                # # 텐서를 넘파이 배열로 변환
+                # outputs_numpy = outputs.cpu().detach().numpy()
+
+                # # 넘파이 배열의 내용 확인
+                # print(outputs_numpy)
     
+                # 손실 함수
+                # PyTorch는 브로드캐스팅(Broadcasting)을 사용하여 손실 함수를 계산하려고 시도하고, 일치하지 않는 차원을 알맞게 확장합니다.
                 loss = self.criterion(outputs, labels)
                 loss.backward()  # 역전파
                 self.optimizer.step()  # 가중치 없데이트
                 
                 preds = torch.argmax(outputs, dim=1)  # output으로부터 class에 대한 예측값을 얻음
+                # dim=1은 텐서의 두 번째 차원(클래스 수에 해당하는 차원)을 따라 최댓값을 찾으라는 의미(클래스 수) -> 가장 높은 클래스의 확률의 인덱스를 선택하여 3차원으로 준다
                 
                 train_loss += loss.item()
                 corrects = torch.sum(preds == labels.data)  # labels와 preds는 이미지 형태
@@ -68,7 +92,7 @@ class SegLearner:
                 # 총 픽셀개수로 나눠줘서 맞은 비율(정확도)를 구한다
                 batch_size = inputs.size(0)
                 train_corr = corrects / (batch_size * 520 * 520)
-                # 앞에 2를 곱하는 이유는 for문 한번에 batch_size만큼 들어가기 때문에 총 픽셀값에 batch_size를 곱한다.
+                # 앞에 2를 곱하는 이유는 for문 한번에 batch_size만큼 들어가기 때문에 총 픽셀값에 batch_size를 곱한다. (여기서 batch_size = 2)
                 
                 train_iou += self.calc_iou(preds, labels.data)
             
@@ -123,15 +147,20 @@ class SegLearner:
            
     def load_ckpts(self):
         # path: .pt 파일이 저장된 위치
-        
         ckpt_path = os.path.join(self.args.weight_folder, self.args.weight_file_name)
-        # 터미널 인자 args로부터 기본값으로 지정된 weight 로딩 경로를 받아옴
         
-        ckpt = torch.load(ckpt_path) # .pt 파일을 불러와서 dictionary 형태로 선언
-        self.model.load_state_dict(ckpt["model"]) # dict 안에 있는 "model"키로 저장할 모델 가중치 로드
-        self.optimizer.load_state_dict(ckpt["optimizer"])
-        self.start_epoch = ckpt["epoch"]
-        self.metrics = ckpt["metrics"]
+        # 터미널 인자 args로부터 기본값으로 지정된 weight 로딩 경로를 받아옴
+        if os.path.isfile(ckpt_path):
+            ckpt = torch.load(ckpt_path) # .pt 파일을 불러와서 dictionary 형태로 선언
+            self.model.load_state_dict(ckpt["model"]) # dict 안에 있는 "model"키로 저장할 모델 가중치 로드
+            self.optimizer.load_state_dict(ckpt["optimizer"])
+            self.start_epoch = ckpt["epoch"]
+            self.metrics = ckpt["metrics"]
+            print(f"Loaded checkpoint from epoch {self.start_epoch}")
+        
+        else:
+            print("Checkpoint file not found. Starting from epoch 0.")
+
         
         
         
@@ -160,6 +189,7 @@ class SegLearner:
         
         
     @staticmethod
+    # 영역 대비 정답률 계산 함수
     def calc_iou(preds, labels):
         
         total_iou = 0.0
