b7bbe7fc5dabc9dfb1cd112207617e0ebf6104fd
이상윤 <speedp001@naver.com>
2023-11-10 01:05:52 +0800

    <glass_project - 학습 진행 코드>

---
+++ glass_project/ultralytics/train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# train model info
+model = YOLO('yolov8s.pt')
+
+if __name__ == '__main__':
+    model.train(data="glass.yaml", epochs=100, batch=16, degrees=5, lrf=0.025, device="mps")
+
